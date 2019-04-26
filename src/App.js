db336ab1382019fe8d571852f21c0373cd78014b
Konrad Siczek <konradd.siczek@gmail.com>
2019-04-26 15:15:55 +0200

    Handled error of non iterable value

--- src/App.js
+++ src/App.js
@@ -7,34 +7,78 @@ import Spinner from './components/Spinner/Spinner'
 class App extends Component {
     state = {
         filteredBooks: [],
+        startIndex : 0,
+        lastPhrase: '',
+        fetched: false,
+        booksLoaded: 0,
+        totalItems: 0,
     }
 
     componentDidMount = () => {
-        this.fetchInitialBooks();
-    }
+        window.addEventListener('scroll', this.handleMoreBooks);
 
-   
-    fetchInitialBooks = () => {
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=search+terms`)
-            .then(response => response.json())
-            .then(responseData => {
-                this.setState({
-                    filteredBooks: responseData.items,
-                }) 
-            })
-    }       
+    }
+  
 
     preformSearch = () => {
         const input = ReactDOM.findDOMNode(this.refs.search);
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=${input.value.toLowerCase()}&maxResults=10`)
-             .then(response => response.json())
-             .then(responseData => {
-                     this.setState((prevState) => ({
-                         filteredBooks: responseData.items,
-                     }))
-                 input.value = ''
-             })
-             
+        fetch(
+          `https://www.googleapis.com/books/v1/volumes?q=${input.value.toLowerCase()}&maxResults=10`
+        )
+          .then(response => {
+            if (!response.ok) {
+              throw Error(response.statusText);
+            }
+            return response.json();
+          })
+          .then(responseData => {
+            this.setState(prevState => ({
+              filteredBooks: responseData.items,
+              startIndex: prevState.startIndex + 10,
+              lastPhrase: input.value.toLowerCase(),
+              fetched: !prevState.fetched,
+              totalItems: responseData.totalItems,
+              booksLoaded: prevState.booksLoaded + 10
+            }));
+            input.value = "";
+            console.log(this.state.booksLoaded);
+            console.log(this.state.totalItems);
+          })
+          .catch(error => console.log(error));
+        this.setState(prevState => ({fetched: !prevState.fetched}));
+        
+    }
+
+    handleMoreBooks = () => {
+      if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight && this.state.totalItems > this.state.booksLoaded) {
+        fetch(
+          `https://www.googleapis.com/books/v1/volumes?q=${
+            this.state.lastPhrase
+          }&maxResults=40&startIndex=${this.state.startIndex}`
+        )
+          .then(response => {
+            if (!response.ok) {
+              throw Error(response.statusText);
+            }
+            return response.json();
+          })
+          .then(json => {
+            const newBooks = json.items.filter(item => {
+                return this.state.filteredBooks.indexOf(item) === -1;
+            })
+            this.setState(prevState => ({
+              filteredBooks: [...prevState.filteredBooks].concat(newBooks),
+              startIndex: prevState.startIndex + 40,
+              fetched: !prevState.fetched,
+              booksLoaded: prevState.booksLoaded + 40
+            }));
+            console.log(this.state.booksLoaded);
+          })
+          .catch(error => console.log(error));
+      } else if(this.state.totalItems <= this.state.booksLoaded ) {
+          console.log('all books has been loaded')
+      }
+        
     }
 
     render() {
--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -5,28 +5,32 @@ class BooksList extends Component {
     render() {
         const {books} = this.props 
         return (
-            <div className={styles.BooksList}>
-                {books.map((book) => (
-                    <div className={styles.Book} key={book.id}>
-                        {
-                            !book.volumeInfo.imageLinks
-                                ?
-                                <div className={`${styles.noImage} ${styles.imageSide}`} />
-                                :
-                                <img src={book.volumeInfo.imageLinks.thumbnail}
-                                    className={`${styles.imageSide} ${styles.image}`}
-                                    alt={book.volumeInfo.title}
-                                />
-                        }
-                        <div>
-                            <h3>{book.volumeInfo.title}</h3>
-                            <p>{!book.volumeInfo.description ? 'No description' : book.volumeInfo.description.substr(0, 300)}</p>
-                        </div>
-
-                    </div>
-                ))}
-            </div>
-        )
+          <div className={styles.BooksList}>
+            {books.map(book => (
+              <div className={styles.Book} key={book.id}>
+                {!book.volumeInfo.imageLinks ? (
+                  <div
+                    className={`${styles.noImage} ${styles.imageSide}`}
+                  />
+                ) : (
+                  <img
+                    src={book.volumeInfo.imageLinks.thumbnail}
+                    className={`${styles.imageSide} ${styles.image}`}
+                    alt={book.volumeInfo.title}
+                  />
+                )}
+                <div>
+                  <h3>{book.volumeInfo.title}</h3>
+                  <p>
+                    {!book.volumeInfo.description
+                      ? "No description"
+                      : book.volumeInfo.description.substr(0, 300)}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        );
     }
    
 }
